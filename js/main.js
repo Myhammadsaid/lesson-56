119a73039b5d4c1bd4d3f26b3b5ad4b93cc86770
Mуhammadsaid <muhammadsaidmm4@gmail.com>
2024-03-20 14:09:40 +0300

    appdat

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+import { product } from "./app.js";
+console.log(product);
+
 function toggleNavbar() {
   document.getElementById("navbar-responsive").classList.toggle("open");
 }
@@ -22,3 +25,42 @@ function toggleBacktop() {
     backtop.style.bottom = "-50px";
   }
 }
+
+//////////////////////////////////////////////////////////////////
+
+const card = document.querySelector(".cards");
+
+function createCard(data) {
+  while (card.firstChild) {
+    card.firstChild.remove();
+  }
+  data.forEach((el) => {
+    let div = document.createElement("div");
+    div.innerHTML = `
+    <div class="card">
+        <img src="${el.thumbnail}" alt="Rectanalage" />
+                <div class="card-text">
+                  <img src="./images/close.png" alt="close" />
+                  <p class="card-text2">Нет в наличии</p>
+                </div>
+                <img class="gift" src="./images/gift.png" alt="gift" />
+                <button class="card-btn">SALE</button>
+                <div class="card-comment">
+                  <div class="stars">
+                    <img src="./images/stars.png" alt="stars" />
+                    <p class="card-text3">(12) отзывов</p>
+                  </div>
+                  <p class="card-text4">${el.description}</p>
+                  <div class="costs">
+                    <p class="card-cost">${el.price}$</p>
+                    <s class="card-cost2">${el.discountPercenteng}$</s>
+                  </div>
+                </div>
+              </div>
+    `;
+
+    card.appendChild(div);
+  });
+}
+
+createCard(product);
